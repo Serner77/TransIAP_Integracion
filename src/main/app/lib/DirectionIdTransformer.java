342bde4576661a62435553c0b1921fbc9c3d4363
NicoleValeria1 <nv.martinezv1@uniandes.edu.co>
2025-11-08 18:28:38 +0100

    Arreglo ID direccion

--- src/main/app/lib/DirectionIdTransformer.java
+++ src/main/app/lib/DirectionIdTransformer.java
@@ -3,7 +3,7 @@ package lib;
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageTransformer;
-
+import java.util.Map;
 import java.util.UUID;
 
 public class DirectionIdTransformer extends AbstractMessageTransformer {
@@ -12,11 +12,28 @@ public class DirectionIdTransformer extends AbstractMessageTransformer {
     public Object transformMessage(MuleMessage message, String outputEncoding) 
             throws TransformerException {
         
-        String directionId = UUID.randomUUID().toString();
+        String directionId;
+        
+        try {
+            Map<String, Object> contenedor = (Map<String, Object>) message.getInvocationProperty("contenedor");
+            
+            String direccion = (String) contenedor.get("trasladoDireccion");
+            String codigoPostal = (String) contenedor.get("trasladoCodigoPostal");
+            String ciudad = (String) contenedor.get("trasladoCiudad");
+            String pais = (String) contenedor.get("trasladoPais");
+            
+            String directionKey = (direccion + "|" + codigoPostal + "|" + ciudad + "|" + pais).toLowerCase();
+            directionId = "dir_" + Math.abs(directionKey.hashCode());
+            
+            System.out.println("Persistent Direction ID: " + directionId + " for: " + directionKey);
+            
+        } catch (Exception e) {
+            directionId = UUID.randomUUID().toString();
+            System.out.println("Error obteniendo datos, usando UUID: " + directionId);
+            e.printStackTrace();
+        }
         
         message.setInvocationProperty("generatedDirectionId", directionId);
-
-        System.out.println("Generated Direction ID: " + directionId);
         
         return message;
     }
